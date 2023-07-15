96a1a5ac957782c2be950227aae5ac9ae86d8a70
purposeTesting <purposetesting92@gmail.com>
2023-07-15 23:42:34 +0530

    explaination of package and module in python

---
+++ package and module/main.py
@@ -0,0 +1,15 @@
+# import func
+# import func as a
+# from func import Func1
+# from .func import Func1 # not allowed
+
+# from python_package import pack1
+# from python_package.pack1 import Substract
+# from .python_package.pack1 import Substract # not allowed
+
+# func.Func1("Vikash")
+# a.Func1("sanu")
+# Func1("mayank")
+
+# pack1.Substract(12, 5)
+Substract(12, 3)
---
+++ package and module/python_package/pack1.py
@@ -0,0 +1,3 @@
+def Substract(num1, num2):
+    c = num1 - num2
+    print(c)
